01d60b060205865df60563d3c520d011aeb22a9c
pabloluqueromero <Pablo.Luque3@alu.uclm.es>
2021-02-12 21:43:25 +0100

    Addin njit

--- tfg/naive_bayes/_naive_bayes.py
+++ tfg/naive_bayes/_naive_bayes.py
@@ -15,7 +15,7 @@ from tfg.encoder import CustomOrdinalFeatureEncoder
 """
 Enhanced methods with Numba nopython mode
 """
-# @njit
+@njit
 def _get_tables(X: np.array, y: np.array , n_classes: int, alpha: float):
     """Computes conditional log count for each value of each feature"""
     smoothed_log_counts = []
@@ -33,7 +33,7 @@ def _get_tables(X: np.array, y: np.array , n_classes: int, alpha: float):
         smoothed_log_counts.append(np.log(smoothed_count))
     return smoothed_log_counts,np.array(feature_values_count),feature_values_count_per_element,np.array(feature_unique_values_count)
 
-# @njit
+@njit
 def _get_counts(column: np.ndarray, y: np.ndarray, n_features: int, n_classes: int):
     """Computes count for each value of each feature for each class value"""
     counts = np.zeros((n_features, n_classes))
@@ -41,7 +41,7 @@ def _get_counts(column: np.ndarray, y: np.ndarray, n_features: int, n_classes: i
         counts[column[i], y[i]] += 1
     return counts
 
-# @njit
+@njit
 def compute_total_probability_(class_values_count_,feature_values_count_,alpha):
     """Computes count for each value of each feature for each class value"""
     total_probability_ = class_values_count_ + alpha*feature_values_count_.reshape(-1,1)

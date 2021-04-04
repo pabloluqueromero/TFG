4d87c493ea5a4631511d7eda5a14bdbc09ae4101
pabloluqueromero <Pablo.Luque3@alu.uclm.es>
2021-04-04 11:05:20 +0200

    Fix nested sequence numpy array

--- tfg/feature_construction/_constructor.py
+++ tfg/feature_construction/_constructor.py
@@ -13,18 +13,17 @@ def get_unique_combinations(X):
 def get_unique_combinations_from_unique_values(X):
     '''Gets all the possible pairs of values vi and vj such that vi belong to Xi and vj belongs to Xj for every pair of features '''
     # return np.unique(X,axis=0) #-> Slower but might produce less combinations (only combinations that appear in the database)
-    return list(product(*[ X[:,j] for j in range(X.shape[1])])) # All possible combinations even though some might not appear
+    return list(product(*[ X[j] for j in range(len(X))])) # All possible combinations even though some might not appear
 
 def construct_features(X,operators=('AND','OR','XOR')):
     '''For each combination returned by get_unique_combinations it generates 3 features with the specified operators)'''
     X_unique = []
     for j in range(X.shape[1]):
         X_unique.append(pd.unique(X[:,j]))
-    X_unique = np.array(X_unique)
     feature_combinations = combinations(np.arange(0,X.shape[1]),2)#combinations_without_repeat(range(X))
     #Generate list:
         # ((attribute1_index,attribute2_index),possiblecombinations)
-    values_combinations = ((index,get_unique_combinations_from_unique_values(X_unique[:,index])) for index in feature_combinations) 
+    values_combinations = ((index,get_unique_combinations_from_unique_values((X_unique[index[0]],X_unique[index[1]]))) for index in feature_combinations) 
     #Add OR operator within the same feature:
     # values =
     constructed_features = product(operators,values_combinations)
